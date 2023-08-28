f002b508461cd979e0af1a0e32ec2bd09928f15a
Maciej Wyrzykowski <maciej_tbg@wp.pl>
2023-08-28 22:31:41 +0200

    Application works fine. No README and tests.

--- src/main/java/io/lsn/SortElements.java
+++ src/main/java/io/lsn/SortElements.java
@@ -5,15 +5,23 @@ import java.util.*;
 public class SortElements {
     public static void main(String[] args) {
         List<Integer> inputList = getIntegerList();
-        List<Integer> sortedListAscending = sortListAscending(inputList);
+        sortListAscending(inputList); // sort list without using Stream API to decrease libraries usage
 
-        System.out.println(inputList);
-        System.out.println(sortedListAscending);
+        System.out.println("Input list: "+inputList);
+        System.out.println("Sorted list: "+inputList);
+        System.out.println("List count: "+inputList.size());
+        System.out.println("Distinct: "+getDistinctElements(inputList));
+        System.out.println("Min value: "+inputList.get(0));
+        System.out.println("Max value: "+inputList.get(inputList.size()-1));
     }
 
-    private static List<Integer> sortListAscending(List<Integer> inputList) {
-        List<Integer> sortedList = new ArrayList<>();
-        return sortedList;
+    private static int getDistinctElements(List<Integer> sortedListAscending) {
+        Set<Integer> distinctElements = new HashSet<>(sortedListAscending);
+        return distinctElements.size();
+    }
+
+    private static void sortListAscending(List<Integer> inputList) {
+        Collections.sort(inputList);
     }
 
     private static List<Integer> getIntegerList() {
@@ -21,9 +29,9 @@ public class SortElements {
         Scanner scanner = new Scanner(System.in);
         List<Integer> inputList = new ArrayList<>();
         String userInput;
-        Integer variable;
+        int variable;
 
-        while (true) {
+        while (true) { // while loop can be broken from inside
             userInput = scanner.nextLine();
             if (userInput.isEmpty()) {
                 break;
